0dc19c1ff9d926d46b0595d0fd1196f358a0917d
Javi <javiprendo@gmail.com>
2015-01-10 22:45:07 +0100

    Filter fixes

--- latch-portlet/src/main/java/com/elevenpaths/latch/web/filter/LatchFilter.java
+++ latch-portlet/src/main/java/com/elevenpaths/latch/web/filter/LatchFilter.java
@@ -1,8 +1,6 @@
 package com.elevenpaths.latch.web.filter;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -28,62 +26,45 @@ public class LatchFilter implements Filter {
 	private static Log log = LogFactoryUtil.getLog(LatchFilter.class);
 	private String secondFactorUrl;
 	private String accountLockedUrl;
-	private final Set<String> internalUrls = new HashSet<String>();
 
 	public void init(FilterConfig config) throws ServletException {
 		secondFactorUrl = config.getInitParameter("latch-second-factor-url");
 		accountLockedUrl = config.getInitParameter("latch-account-locked-url");
-		internalUrls.add( get2FactorPath() );
-		internalUrls.add( getAccountLockedPath() );
-		internalUrls.add( PortalUtil.getPathMain()+"/portal/logout" );
 	}
 	
 	public void doFilter(ServletRequest sRequest, ServletResponse sResponse, FilterChain chain) throws IOException, ServletException {
-		if( sRequest instanceof HttpServletRequest ){
-			try {
-				HttpServletRequest request = (HttpServletRequest) sRequest;
-				HttpServletResponse response = (HttpServletResponse) sResponse;
-				HttpSession session = request.getSession(false);
-				String uri = request.getRequestURI();
-				
-				if( session!=null && LatchConfig.isConfigured() && !isInternalUrl(uri) ){
-					//Checks if account is locked
-					boolean accountLocked = session.getAttribute(LatchKeys.SESSION_ACCOUNT_LOCKED)!=null;
-					if( accountLocked ){
-						response.sendRedirect( getAccountLockedPath() );
-					}
-					
-					//Checks if has 2 factor
-					boolean hasToken = session.getAttribute(LatchKeys.SESSION_ACCOUNT_2_FACTOR)!=null;
-					if( hasToken ){
-						response.sendRedirect( get2FactorPath() );
-					}
+		boolean doChain = true;
+		try {
+			HttpServletRequest request = (HttpServletRequest) sRequest;
+			HttpServletResponse response = (HttpServletResponse) sResponse;
+			HttpSession session = request.getSession(false);
+			
+			if( session!=null && LatchConfig.isConfigured() ){
+				//Checks if account is locked
+				boolean accountLocked = session.getAttribute(LatchKeys.SESSION_ACCOUNT_LOCKED)!=null;
+				if( accountLocked ){
+					response.sendRedirect( getAccountLockedPath() );
+					doChain = false;
 				}
-			} catch (Exception e) {
-				log.error("Error executing Latch filter: ", e);
+				
+				//Checks if has 2 factor
+				boolean hasToken = session.getAttribute(LatchKeys.SESSION_ACCOUNT_2_FACTOR)!=null;
+				if( hasToken ){
+					response.sendRedirect( get2FactorPath() );
+					doChain = false;
+				}
 			}
+		} catch (Exception e) {
+			log.error("Error executing Latch filter: ", e);
 		}
 		
-		chain.doFilter(sRequest, sResponse);
+		if(doChain){
+			chain.doFilter(sRequest, sResponse);
+		}
 	}
 	
 	public void destroy() {
 		log.info("Latch Filter is down");
-	}
-	
-	/**
-	 * Check if requested URL is any of the URLs used by Latch Plugin 
-	 * @param uri
-	 * @return true if is internal URL
-	 */
-	private boolean isInternalUrl(String uri){
-		for(String path : internalUrls){
-			if( path.equals(uri) ){
-				return true;
-			}
-		}
-		
-		return false;
 	}
 	
 	/**
